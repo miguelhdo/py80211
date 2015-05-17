a6ef2bbf4ae71110c492fb3d086c7be0b4f38d1e
Arend van Spriel <aspriel@gmail.com>
2015-05-17 19:38:33 +0200

    py80211: examples: remove unused function
    The find_ie function was no longer used so remove it. It is
    actually being moved to cli module.
    
    Signed-off-by: Arend van Spriel <aspriel@gmail.com>

--- examples/example02.py
+++ examples/example02.py
@@ -3,15 +3,6 @@ import netlink.capi as nl
 from py80211.cli import bss_info
 import sys
 
-def find_ie(ies, eid):
-	while len(ies) > 2 and ies[0] != eid:
-		ies = ies[ies[1]+2:]
-	if len(ies) < 2:
-		return None
-	if len(ies) < 2 + ies[1]:
-		return None
-	return ies[0:2+ies[1]]
-
 ifidx = nl.if_nametoindex(sys.argv[1])
 rh = scan_request(ifidx)
 rh.add_ssids(['Ziggo'])
